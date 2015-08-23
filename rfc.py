8e28356df0da0db6e35819a6108a7dbebab1298f
Stefan Kopieczek <stefankopieczek@gmail.com>
2015-08-23 14:13:43 +0100

    Add caching support
    Allow downloading of zip files to an offline cache via the -d argument.

--- rfc.py
+++ rfc.py
@@ -1,25 +1,46 @@
+import argparse
 import pydoc
-import sys
 
 try:
     from urllib.request import urlopen
 except ImportError:
     from urllib2 import urlopen
 
+import rfc_cache
 
-def get_rfc(rfc):
-    url = "http://www.ietf.org/rfc/rfc{0}.txt".format(rfc)
-    f = urlopen(url)
-    data = f.read()
-    if isinstance(data, bytes):
-        data = data.decode('utf-8')
 
-    return data
+def get_rfc(rfc_number, use_cache=True):
+    rfc = None
+
+    if use_cache:
+        rfc = rfc_cache.get_from_cache(rfc_number)
+
+    if rfc is None:
+        url = "http://www.ietf.org/rfc/rfc{0}.txt".format(rfc_number)
+        f = urlopen(url)
+        rfc = f.read()
+        if isinstance(rfc, bytes):
+            rfc = rfc.decode('utf-8')
+
+    return rfc
 
 
 def render_rfc(rfc):
-    pydoc.pager(get_rfc(rfc))
+    pydoc.pager(rfc)
+
+
+def parse_args():
+    parser = argparse.ArgumentParser(
+        description='Download and browse RFCs, or save to read later')
+    parser.add_argument('-d', '--download', action='store_true',
+                        help='Save the RFC to read later')
+    parser.add_argument('rfc', type=int)
+    return parser.parse_args()
 
 
 if __name__ == "__main__":
-    render_rfc(sys.argv[1])
+    args = parse_args()
+    if args.download:
+        rfc_cache.add_to_cache(args.rfc, get_rfc(args.rfc, use_cache=False))
+    else:
+        render_rfc(get_rfc(args.rfc))
---
+++ rfc_cache.py
@@ -0,0 +1,63 @@
+import os
+import logging
+import sys
+from zipfile import ZipFile
+
+from exceptions import CriticalError
+
+
+def add_to_cache(rfc_num, rfc_text):
+    with ZipFile(get_rfc_cache(), 'a') as cache:
+        cache.writestr('rfc{0}.txt'.format(rfc_num), rfc_text)
+    logging.debug('Cached RFC {0} to RFC cache file'.format(rfc_num))
+
+
+def get_from_cache(rfc_num):
+    try:
+        with ZipFile(get_rfc_cache(), 'r') as cache:
+            with cache.open('rfc{0}.txt'.format(rfc_num), 'r') as rfc:
+                data = rfc.read()
+                if isinstance(data, bytes):
+                    data = data.decode('utf-8')
+                return data
+    except KeyError:
+        return None
+
+
+def get_appdata_folder():
+    if sys.platform.startswith('linux'):
+        return os.getenv('HOME')
+    elif sys.platform in ['win32', 'cygwin']:
+        return os.getenv('APPDATA')
+    elif sys.platform == 'darwin':
+        return os.path.expanduser('~/Library/Application Support')
+    else:
+        logging.warning('Unsupported platform {0}; '
+                        'RFC storage may fail'.format(sys.platform))
+        return os.getenv('HOME')
+
+
+def get_rfc_cache():
+    appdata_folder = get_appdata_folder()
+    if not os.path.exists(appdata_folder):
+        raise CriticalError('Unable to find appdata folder; '
+                            '{0} does not exist'.format(appdata_folder))
+
+    rfc_cache_folder = os.path.join(appdata_folder, '.rfccache')
+    if not os.path.exists(rfc_cache_folder):
+        os.mkdir(rfc_cache_folder)
+        logging.debug('Created rfc cache folder at {0}'.format(
+            rfc_cache_folder))
+    elif not os.path.isdir(rfc_cache_folder):
+        raise CriticalError('Unable to create rfc cache folder {0}; '
+                            'a file already exists with that name'.format(
+                                rfc_cache_folder))
+
+    rfc_cache = os.path.join(rfc_cache_folder, 'rfcs.zip')
+    if not os.path.exists(rfc_cache):
+        # The RFC cache zip does not exist; create an empty zip.
+        zf = ZipFile(rfc_cache, 'w')
+        zf.close()
+        logging.debug('Created rfc cache at {0}'.format(rfc_cache))
+
+    return rfc_cache
